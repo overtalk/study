e3e8863c2d7247ebccdbc378b9ec49e32eadec1d
秦翰 <qinhan@xindong.com>
2019-05-23 18:00:23 +0800

    feat

---
+++ go/context/context_muli.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+var (
+	key1 = "key1"
+	key2 = "key2"
+	key3 = "key3"
+)
+
+func main2() {
+	ctx1 := context.WithValue(context.Background(), key1, "1")
+	ctx2 := context.WithValue(ctx1, key2, "2")
+	ctx3 := context.WithValue(ctx2, key3, "3")
+
+	value1 := ctx3.Value(key1)
+	value2 := ctx3.Value(key2)
+	value3 := ctx3.Value(key3)
+
+	fmt.Println(value1, value2, value3)
+}
