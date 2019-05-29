d2e7b489e0537be0371ead4509dca2f8bb90cb1f
秦翰 <qinhan@xindong.com>
2019-05-29 17:09:32 +0800

    feat(mongodb): add mongodb

--- mongodb/connect.go
+++ mongodb/connect.go
@@ -9,7 +9,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type DBInfo struct {
+// MongoConf : config of mongodb
+type MongoConf struct {
 	Size          uint16
 	Addr          []string
 	Username      string
@@ -19,34 +20,32 @@ type DBInfo struct {
 	AuthMechanism string
 }
 
-type Pool = mongo.Database
-
-func Connect(info DBInfo) (*Pool, error) {
-	if info.Size < 1 {
-		log.Printf("invalid mysql pool size: %d", info.Size)
+// Connect : connect mongo db
+func (m *MongoConf) Connect() (*mongo.Database, error) {
+	if m.Size < 1 {
+		log.Printf("invalid mysql pool size: %d", m.Size)
 		return nil, errors.New("invalid mysql pool size")
 	}
 	option := options.Client()
-	option.SetHosts(info.Addr)
+	option.SetHosts(m.Addr)
 	option.SetAuth(options.Credential{
 		PasswordSet:   true,
-		Username:      info.Username,
-		Password:      info.Password,
-		AuthMechanism: info.AuthMechanism,
+		Username:      m.Username,
+		Password:      m.Password,
+		AuthMechanism: m.AuthMechanism,
 	})
-	option.SetMaxPoolSize(info.Size)
-	option.SetReplicaSet(info.ReplicaSet)
+	option.SetMaxPoolSize(m.Size)
+	option.SetReplicaSet(m.ReplicaSet)
 
 	pool, err := mongo.NewClient(option)
 	if err != nil {
-		log.Printf("failed to new client to mongodb [%v]: %v", info.Addr, err)
+		log.Printf("failed to new client to mongodb [%v]: %v", m.Addr, err)
 		return nil, err
 	}
 	err = pool.Connect(context.Background())
 	if err != nil {
-		log.Printf("failed to connect to mongodb [%v]: %v", info.Addr, err)
+		log.Printf("failed to connect to mongodb [%v]: %v", m.Addr, err)
 		return nil, err
 	}
-	database := pool.Database(info.DBName)
-	return database, nil
+	return pool.Database(m.DBName), nil
 }
--- mongodb/connect_test.go
+++ mongodb/connect_test.go
@@ -1,1 +1,46 @@
 package mongodb_test
+
+import (
+	// "context"
+	"testing"
+	// "time"
+
+	"github.com/qinhan-shu/study/mongodb"
+)
+
+func TestConnect(t *testing.T) {
+	conf := &mongodb.MongoConf{
+		Size:          10,
+		Addr:          []string{"localhost:27017"},
+		Username:      "test",
+		Password:      "test",
+		DBName:        "test",
+		AuthMechanism: "SCRAM-SHA-1",
+	}
+
+	db, err := conf.Connect()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cancel()
+
+	where := make(map[string]interface{})
+	where["q"] = "q"
+	results := db.Collection("aaa").FindOne(nil, where)
+	err = results.Err()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	newData := new(map[string]interface{})
+	if err := results.Decode(newData); err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(newData)
+
+}
